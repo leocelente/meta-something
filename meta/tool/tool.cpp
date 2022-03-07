88f45a37433f206c7b45d4ac992f6a7aeb57c424
Leonardo Celente <leocelente@gmail.com>
2022-03-07 11:18:06 -0300

    Updated messy parsing logic to simpler OOP interface

--- meta/tool/tool.cpp
+++ meta/tool/tool.cpp
@@ -1,64 +1,162 @@
 #include "tool.hpp"
 #include "clang-c/Index.h"
+#include <algorithm>
 #include <cstring>
-#include <fstream>
-#include <iostream>
-#include <string>
+#include <tuple>
 
-#define NAMEOF(cursor) clang_getCString(clang_getCursorSpelling(cursor))
-#define KINDOF(cursor)                                                         \
-  clang_getCString(clang_getCursorKindSpelling(clang_getCursorKind(cursor)))
+// TODO: Consolidate get_methods and get_fields visitors on a template
 
-std::ostream &operator<<(std::ostream &stream, const CXString &str) {
-  stream << clang_getCString(str);
-  clang_disposeString(str);
-  return stream;
+#define StringIsSame(left, right) strcmp(left, right) == 0
+
+Scanner::Scanner(std::string_view filename) {
+  CXIndex index = clang_createIndex(0, 0);
+  CXTranslationUnit unit = clang_parseTranslationUnit(
+      index, filename.data(), nullptr, 0, nullptr, 0, CXTranslationUnit_None);
+  if (unit == nullptr) {
+    throw "Failed parse File";
+  }
+  auto cursor = clang_getTranslationUnitCursor(unit);
+  memcpy(&this->m_root, &cursor, sizeof(CXCursor));
+  memcpy(&this->m_index, &index, sizeof(CXIndex));
+  memcpy(&this->m_unit, &unit, sizeof(CXTranslationUnit));
+
+  this->get_target();
+  this->get_methods();
+  this->get_fields();
 }
 
-CXCursor system_c{};
-
-CXChildVisitResult getMethods(CXCursor cursor, CXCursor parent,
-                              CXClientData clientData) {
-  if (strcmp(NAMEOF(cursor), NAMEOF(system_c)) == 0) {
-    return CXChildVisit_Recurse;
-  }
-  if (strcmp(KINDOF(cursor), "CXXMethod") == 0) {
-    std::cout << "  // Found Method: " << NAMEOF(cursor) << '\n';
-  }
-  return CXChildVisit_Continue;
+Scanner::~Scanner() {
+  clang_disposeTranslationUnit(this->m_unit);
+  clang_disposeIndex(this->m_index);
 }
 
-CXChildVisitResult getFields(CXCursor cursor, CXCursor parent,
-                             CXClientData clientData) {
-  if (strcmp(NAMEOF(cursor), NAMEOF(system_c)) == 0) {
-    return CXChildVisit_Recurse;
-  }
-  if (strcmp(KINDOF(cursor), "FieldDecl") == 0) {
-    std::cout << "  // Found Field: " << NAMEOF(cursor) << '\n';
-  }
-  return CXChildVisit_Continue;
+std::string Scanner::get_name(CXCursor const cursor) {
+  return std::string{clang_getCString(clang_getCursorSpelling(cursor))};
 }
 
-CXChildVisitResult findSystem(CXCursor cursor, CXCursor parent,
-                              CXClientData clientData) {
+CursorKind Scanner::get_kind(CXCursor const cursor) {
+  char const *const kindName = clang_getCString(
+      clang_getCursorKindSpelling(clang_getCursorKind(cursor)));
+  if (StringIsSame(kindName, "TypeRef")) {
+    return CursorKind::IsTypeRef;
+  } else if (StringIsSame(kindName, "ClassDecl")) {
+    return CursorKind::IsClass;
+  } else if (StringIsSame(kindName, "StructDecl")) {
+    return CursorKind::IsStruct;
+  } else if (StringIsSame(kindName, "CXXMethod")) {
+    return CursorKind::IsMethod;
+  } else if (StringIsSame(kindName, "FieldDecl")) {
+    return CursorKind::IsField;
+  }
+  return CursorKind::IsUknown;
+}
 
-  if (strcmp(NAMEOF(parent), "main") == 0) {
-    std::cout << "// Inside main\n";
-    return CXChildVisit_Recurse;
-  } else {
-    if (strcmp(NAMEOF(cursor), "Generate") == 0) {
-      std::cout << "// Found Generate " << KINDOF(cursor) << '\n';
+std::vector<std::string> Scanner::get_methods() const {
+  std::vector<std::string> methods{};
+
+  auto target_name = Scanner::get_name(m_target);
+  auto visitor =
+      [/*must be empty*/](CXCursor cursor, CXCursor parent,
+                          CXClientData clientData) -> CXChildVisitResult {
+    auto pData = static_cast<
+        std::tuple<std::string *,             // pointer to target_name string
+                   std::vector<std::string> * // pointer to methods vector
+                   > * /* parameter is passed as void pointer */>(clientData);
+
+    auto [target_name, methods] = *pData;
+    // make sure we are inside the target
+    auto cursor_name = Scanner::get_name(cursor);
+    if (cursor_name == *target_name) {
       return CXChildVisit_Recurse;
     }
-  }
-  if (strcmp(KINDOF(cursor), "TypeRef") == 0) {
-    std::cout << "// System is referenced as: "
-              << clang_getCursorSpelling(cursor) << '\n';
-    CXCursor actual_system = clang_getCursorDefinition(cursor);
-    memcpy(&system_c, &actual_system, sizeof(CXCursor));
-    return CXChildVisit_Break;
-  }
-  return CXChildVisit_Recurse;
+
+    // match only methods
+    auto cursor_kind = Scanner::get_kind(cursor);
+    if (cursor_kind == CursorKind::IsMethod) {
+      // add method name to vector
+      methods->emplace_back(cursor_name);
+      std::cout << "// method: " << cursor_name << '\n';
+      return CXChildVisit_Continue;
+    }
+    return CXChildVisit_Continue;
+  };
+
+  auto param = std::make_tuple(&target_name, &methods);
+  /* Call to visitChildren will modify the cursor, so we'll use a copy */
+  CXCursor start{};
+  memcpy(&start, &this->m_target, sizeof(CXCursor));
+
+  clang_visitChildren(start, visitor, &param);
+
+  return methods;
+}
+
+std::vector<std::string> Scanner::get_fields() const {
+  std::vector<std::string> fields{};
+
+  auto target_name = Scanner::get_name(m_target);
+  auto visitor =
+      [/*must be empty*/](CXCursor cursor, CXCursor parent,
+                          CXClientData clientData) -> CXChildVisitResult {
+    auto pData = static_cast<
+        std::tuple<std::string *,             // pointer to target_name string
+                   std::vector<std::string> * // pointer to methods vector
+                   > * /* parameter is passed as void pointer */>(clientData);
+
+    auto [target_name, fields] = *pData;
+    // make sure we are inside the target
+    auto cursor_name = Scanner::get_name(cursor);
+    if (cursor_name == *target_name) {
+      return CXChildVisit_Recurse;
+    }
+
+    // match only methods
+    auto cursor_kind = Scanner::get_kind(cursor);
+    if (cursor_kind == CursorKind::IsField) {
+      // add method name to vector
+      fields->emplace_back(cursor_name);
+      std::cout << "// field: " << cursor_name << '\n';
+      return CXChildVisit_Continue;
+    }
+    return CXChildVisit_Continue;
+  };
+
+  auto param = std::make_tuple(&target_name, &fields);
+  /* Call to visitChildren will modify the cursor, so we'll use a copy */
+  CXCursor start{};
+  memcpy(&start, &this->m_target, sizeof(CXCursor));
+
+  clang_visitChildren(start, visitor, &param);
+
+  return fields;
+}
+
+void Scanner::get_target() {
+  auto visitor = [](CXCursor cursor, CXCursor parent,
+                    CXClientData clientData) -> CXChildVisitResult {
+    auto target = static_cast<CXCursor *>(clientData);
+
+    if (Scanner::get_name(parent) == "main") {
+      return CXChildVisit_Recurse;
+    } else if (Scanner::get_name(cursor) == "Generate") {
+      return CXChildVisit_Recurse; // next is the template parameter
+    }
+    // We expect that now the cursor is pointing to a TypeRef to the actual
+    // target
+    if (Scanner::get_kind(cursor) == CursorKind::IsTypeRef) {
+      // De-reference the type
+      CXCursor actual_system = clang_getCursorDefinition(cursor);
+      // copy cursor pointing to type to m_target
+      memcpy(target, &actual_system, sizeof(CXCursor));
+      return CXChildVisit_Break;
+    }
+    return CXChildVisit_Recurse;
+  };
+
+  CXCursor start{};
+  memcpy(&start, &this->m_root, sizeof(CXCursor));
+
+  clang_visitChildren(start, visitor, &m_target);
 }
 
 int main(int const argc, char const *argv[]) {
@@ -66,32 +164,12 @@ int main(int const argc, char const *argv[]) {
     std::cerr << "Wrong number of arguments!\n";
     std::exit(-1);
   }
-
-  std::string const input_filename{argv[1]};
   std::cout << "template<class T>\nstruct Generate{\n  int operator()() "
                "{\n\t\treturn"
                " 0;\n\t} \n};\n";
 
-  CXIndex index = clang_createIndex(0, 0);
+  std::string const input_filename{argv[1]};
 
-  CXTranslationUnit unit =
-      clang_parseTranslationUnit(index, input_filename.c_str(), nullptr, 0,
-                                 nullptr, 0, CXTranslationUnit_None);
-  if (unit == nullptr) {
-    std::cerr << "Unable to parse translation unit of " << input_filename
-              << ". \nQuitting.\n";
-    std::exit(-1);
-  }
-  CXCursor cursor = clang_getTranslationUnitCursor(unit);
-
-  // find generator
-  clang_visitChildren(cursor, findSystem, nullptr);
-  std::cout << "// System: " << clang_getCursorSpelling(system_c) << '\n';
-
-  clang_visitChildren(cursor, getMethods, nullptr);
-  clang_visitChildren(cursor, getFields, nullptr);
-
-  clang_disposeTranslationUnit(unit);
-  clang_disposeIndex(index);
+  Scanner scanner{input_filename};
   return 0;
 }
--- meta/tool/tool.hpp
+++ meta/tool/tool.hpp
@@ -1,1 +1,46 @@
 #include "clang-c/Index.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <tuple>
+#include <vector>
+
+enum class CursorKind {
+  IsTypeRef,
+  IsStruct,
+  IsClass,
+  IsField,
+  IsMethod,
+  IsUknown = 99
+};
+class Scanner {
+private:
+  /* AST Cursor pointing to translation unit root node */
+  CXCursor m_root{};
+  CXIndex m_index{};
+  CXTranslationUnit m_unit{};
+
+  /* AST Cursor pointing to the class passed to Generator<target> */
+  CXCursor m_target{};
+
+  /* Helper to get Node name from cursor */
+  static std::string get_name(CXCursor const cursor);
+
+  /* Helper to get Node kind aka C++ base type Strct, Class, Field */
+  static CursorKind get_kind(CXCursor const cursor);
+
+  /* Examines target cursor to find it's node's methods */
+  std::vector<std::string> get_methods() const;
+
+  /* Examines target cursor to find it's node's fields (member variables) */
+  std::vector<std::string> get_fields() const;
+
+  /* Finds target from root node */
+  void get_target();
+
+public:
+  /* Extracts the relevant information from cursor of root node of AST */
+  Scanner(std::string_view filename);
+  ~Scanner();
+};
